25c6260bc005d4d09210afbf72c269e22204b57f
Bloodtick Jones <12273404+bloodtick@users.noreply.github.com>
2026-09-25 15:50:59 -0400

    Fix formatting of CONF_UNIT in const.py

--- custom_components/virtual_switch/const.py
+++ custom_components/virtual_switch/const.py
@@ -22,6 +22,5 @@ CONF_ATTRIBUTE = "attribute"
 CONF_COPY_UNIT = "copy_unit"
 CONF_UNIT_ATTRIBUTE = "unit_attribute"
 
-# Optional attribute transformation
 CONF_VALUE_TEMPLATE = "value_template"
 CONF_UNIT = "unit"
